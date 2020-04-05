0f29eb51000e74c8642059076d714ae826588175
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-04-05 04:56:16 +0000

    Fix bugs and conform to pep8

--- producer.py
+++ producer.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Producer
+from confluent_kafka import avro, Producer
 from confluent_kafka.avro import CachedSchemaRegistryClient
 from confluent_kafka.avro.serializer.message_serializer import MessageSerializer
 
@@ -32,6 +32,7 @@ BROKER = os.environ['BROKER']
 SCHEMA_REGISTRY_URL = os.environ['SCHEMA_REGISTRY_URL']
 TOPIC_NAME = os.environ['TOPIC_NAME']
 
+
 def delivery_callback(err, msg):
     """
     Called once for each message produced to indicate delivery result.
@@ -41,20 +42,22 @@ def delivery_callback(err, msg):
         print('Message delivery failed: {}'.format(err))
     else:
         print('Message delivered to {} [{}]'.
-        format(msg.topic(), msg.partition()))
+              format(msg.topic(), msg.partition()))
+
 
 # Create a topic if it doesn't exist yet
+print('Checking if topic: {} exists...'.format(TOPIC_NAME))
 admin = CustomAdmin(BROKER)
 if not admin.topic_exists(TOPIC_NAME):
-  admin.create_topics([TOPIC_NAME])
+    admin.create_topics([TOPIC_NAME])
 
 # Define wrapper function for serializing in avro format
 serialize_avro = MessageSerializer(
     CachedSchemaRegistryClient(SCHEMA_REGISTRY_URL)
-  ).encode_record_with_schema
+).encode_record_with_schema
 
 # Define value schema
-value_schema = """
+value_schema = avro.loads("""
     {
         "namespace": "septa.bus.location",
         "name": "value",
@@ -64,20 +67,24 @@ value_schema = """
             {"name": "lng", "type": "float", "doc": "longitude"}
         ]
     }
-"""
+""")
 
 # Initialize producer
 # Configuration options:
 # https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+print(help(Producer))
 producer = Producer(
-  {
-    'bootstrap.servers': BROKER,
-    # Maximum number of messages to send at once. Limits the size of your
-    # queue. The default is 10000 at the time of this writing (see above link
-    # for updated config). It is set to 1 here so the user can see the output
-    # update per message in the logs.
-    'batch_num_messages': 1,
-  }
+    {
+        'bootstrap.servers': BROKER,
+        # Maximum number of messages to send at once.
+        # Limits the size of your queue.
+        # The default is 10000 at the time of this writing.
+        # See above link for updated defaults.
+        # It is set to 1 here so the user can see the output
+        # update per message in the logs.
+        'batch.num.messages': 1,
+        'default.topic.config': {'acks': 1}
+    }
 )
 
 # Initialize key and values
@@ -88,34 +95,34 @@ lng = -75.18071
 # Produce n events simulating bus movements
 n = 10000
 for i in range(n):
-  value = {
-    "lat": lat,
-    "lng": lng 
-  }
-  serialized_value = serialize_avro(topic=TOPIC_NAME,
-                                    value_schema=value_schema,
-                                    value=value,
-                                    is_key=false)
-  serialized_key = str(key)
-  try:
-    producer.produce(topic=TOPIC_NAME,
-                     value=serialized_value,
-                     key=serialized_key,
-                     callback=delivery_callback)
-  except BufferError:
-    print("Local producer queue is full: {} messages in queue".format(len(producer)))
+    value = {
+        "lat": lat,
+        "lng": lng
+    }
+    serialized_value = serialize_avro(topic=TOPIC_NAME,
+                                      schema=value_schema,
+                                      record=value,
+                                      is_key=False)
+    serialized_key = str(key)
+    try:
+        producer.produce(topic=TOPIC_NAME,
+                         value=serialized_value,
+                         key=serialized_key,
+                         callback=delivery_callback)
+    except BufferError:
+        print("Local producer queue is full: {} messages in queue".format(
+          len(producer)))
 
-  print("I just produced key: {} lat: {}, lng: {}".format(key, lat, lng))
-  # Polls the producer for events and calls the corresponding callbacks
-  # (if registered)
-  #
-  # `timeout` refers to the maximum time to block waiting for events
-  #
-  # Since produce() is an asynchronous API this poll() call will most
-  # likely not serve the delivery callback for the last produce()d message.
-  lat += 0.000001
-  lng += 0.000001
-  producer.poll(timeout=0)
-  time.sleep(1)
-# Cleanup step: wait for all messages to be delivered before exiting. 
+    print("I just produced key: {} lat: {}, lng: {}".format(key, lat, lng))
+    # Polls the producer for events and calls the corresponding callbacks
+    # (if registered)
+    #
+    # `timeout` refers to the maximum time to block waiting for events
+    #
+    # Since produce() is an asynchronous API this poll() call will most
+    # likely not serve the delivery callback for the last produce()d message.
+    lat += 0.000001
+    lng += 0.000001
+    producer.poll(timeout=3)
+# Cleanup step: wait for all messages to be delivered before exiting.
 producer.flush()
