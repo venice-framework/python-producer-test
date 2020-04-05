aee0939027f9b92d23780565cfd7fd1108299cc9
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-04-05 06:00:44 +0000

    Have two options for producer

--- producer_key_str.py
+++ producer_key_str.py
@@ -1,5 +1,6 @@
-from confluent_kafka import avro
-from confluent_kafka.avro import AvroProducer
+from confluent_kafka import avro, Producer
+from confluent_kafka.avro import CachedSchemaRegistryClient
+from confluent_kafka.avro.serializer.message_serializer import MessageSerializer
 
 import os
 import requests
@@ -24,16 +25,25 @@ You must set the TOPIC_NAME environment variable.
 This image does not have a default TOPIC_NAME set, to avoid
 potentially confusing errors.
 
-This version of the producer serializes the key with avro.
+This version of the producer serializes the key as a string and
+the values in avro format, because ksql requires the key to be a string.
 
-For a version that serializes the key as a string, see the repo.
+For a version that serializes the key and values as avro,
+see `producer_key_avro.py` 
+
+References:
+https://github.com/confluentinc/confluent-kafka-python
+https://github.com/confluentinc/confluent-kafka-python/issues/428#issuecomment-444320103
+https://docs.confluent.io/current/clients/confluent-kafka-python/
+
 """
 
 BROKER = os.environ['BROKER']
 SCHEMA_REGISTRY_URL = os.environ['SCHEMA_REGISTRY_URL']
 TOPIC_NAME = os.environ['TOPIC_NAME']
 
-def delivery_report(err, msg):
+
+def delivery_callback(err, msg):
     """
     Called once for each message produced to indicate delivery result.
     Triggered by poll() or flush().
@@ -42,74 +52,68 @@ def delivery_report(err, msg):
         print('Message delivery failed: {}'.format(err))
     else:
         print('Message delivered to {} [{}]'.
-        format(msg.topic(), msg.partition()))
+              format(msg.topic(), msg.partition()))
 
 
 # Create a topic if it doesn't exist yet
 admin = CustomAdmin(BROKER)
 if not admin.topic_exists(TOPIC_NAME):
-  admin.create_topics([TOPIC_NAME])
+    admin.create_topics([TOPIC_NAME])
 
-# Define schemas
-# NOTE: bus_id is included in the value as a hacky workaround
-# because ksql does not recognize avro-encoded keys and
-# AvroProducer does not allow a different encoding for keys at the
-# time of this writing.
-# See https://github.com/confluentinc/confluent-kafka-python/issues/428
+# Define wrapper function for serializing in avro format
+serialize_avro = MessageSerializer(
+    CachedSchemaRegistryClient(SCHEMA_REGISTRY_URL)
+).encode_record_with_schema
+
+# Define value schema
 value_schema = avro.loads("""
     {
         "namespace": "septa.bus.location",
         "name": "value",
         "type": "record",
         "fields": [
-            {"name": "bus_id", "type": "int", "doc": "bus id"},
             {"name": "lat", "type": "float", "doc": "latitude"},
             {"name": "lng", "type": "float", "doc": "longitude"}
         ]
     }
 """)
 
-key_schema = avro.loads("""
-{
-   "namespace": "septa.bus.location",
-   "name": "key",
-   "type": "record",
-   "fields" : [
-     {
-       "name" : "bus_id",
-       "type" : "int"
-     }
-   ]
-}
-""")
-
 # Initialize producer
-avroProducer = AvroProducer(
-  {
-    'bootstrap.servers': BROKER,
-    'on_delivery': delivery_report,
-    'schema.registry.url': SCHEMA_REGISTRY_URL
-  },
-  default_key_schema=key_schema,
-  default_value_schema=value_schema
+# Configuration options:
+# https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+producer = Producer(
+    {
+        'bootstrap.servers': BROKER,
+        'default.topic.config': {'acks': 1}
+    }
 )
 
 # Initialize key and values
+key = 1
 lat = 40.043152
 lng = -75.18071
-bus_id = 1
 
-key = {"bus_id": 1}
-
-# Produce events simulating bus movements, forever
+# Produce events simulating bus movements
 count = 1
 while True:
     value = {
-      "bus_id": bus_id,
-      "lat": lat,
-      "lng": lng 
+        "lat": lat,
+        "lng": lng
     }
-    avroProducer.produce(topic=TOPIC_NAME, value=value, key=key)
+    serialized_value = serialize_avro(topic=TOPIC_NAME,
+                                      schema=value_schema,
+                                      record=value,
+                                      is_key=False)
+    serialized_key = str(key)
+    try:
+        producer.produce(topic=TOPIC_NAME,
+                         value=serialized_value,
+                         key=serialized_key,
+                         callback=delivery_callback)
+    except BufferError:
+        print("Local producer queue is full: {} messages in queue".format(
+          len(producer)))
+
     print("EVENT COUNT: {} key: {} lat: {}, lng: {}".format(count, key, lat, lng))
     # Polls the producer for events and calls the corresponding callbacks
     # (if registered)
